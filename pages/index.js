e6c981d143d2b84fbb2d27b715d44ec84dd79e43
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-05-13 13:54:15 -0500

    Next.js button routing and style applied to index page

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ const Index =()=> {
     return (
       <div>
       <h1>Welcome to my Next.js project</h1>
-      <Link prefetch href="./about"><a>Read about Next</a></Link>
+      <Link prefetch href="./about"><a style={{fontSize : 20}}>Read about Next</a></Link>
+      <p></p>
+      <Link href="/about"><button>Try Me!</button></Link>
       </div>
     )
 }
